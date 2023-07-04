a3f17937490fda4116536d50eb31cfb14a7efb68
Koen van der Heide | Aqualysis <kheide@aqualysis.nl>
2023-07-04 14:47:18 +0200

    AAV-67 group aesthetic toegevoegd aan resultaten grafieken

--- Shiny/app.R
+++ Shiny/app.R
@@ -455,7 +455,7 @@ server <- function(input, output, session) {
     selected_data <- graph_selection() 
     
     results_plot <- ggplot(data = plot_data,
-                   mapping = aes(x = SAMPLINGDATE, y = RESULTAAT, colour = TESTCODE)) +
+                   mapping = aes(x = SAMPLINGDATE, y = RESULTAAT, colour = TESTCODE, group = MONSTERPUNTCODE)) +
       geom_line() +
       geom_point() +
         #aes(shape = selected_data),  
