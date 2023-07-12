5242067e2c46eb034e6a32c049f22cd39b1b0035
Koen van der Heide | Aqualysis <kheide@aqualysis.nl>
2023-07-12 15:07:18 +0200

    AAV-86 ditto voor ratio grafieken

--- Shiny/app.R
+++ Shiny/app.R
@@ -439,8 +439,8 @@ server <- function(input, output, session) {
     results_plot <- ggplot(data = plot_data,
                    mapping = aes(x = SAMPLINGDATE, y = RESULTAAT, colour = NAAM, group = MONSTERPUNTCODE)) +
       geom_line(alpha = 0.7) +
-      geom_point(size = 2.5, alpha = 0.5,aes(shape = GEVALIDEERD)) +
-      geom_point(data = current_data, size = 3.5,aes(shape = GEVALIDEERD)) +
+      geom_point(size = 2.5, alpha = 0.5, aes(shape = GEVALIDEERD)) +
+      geom_point(data = current_data, size = 3.5, aes(shape = GEVALIDEERD)) +
       guides(size = FALSE) +
       facet_wrap(vars(TESTCODE), scales = 'free_y')
     
@@ -465,9 +465,9 @@ server <- function(input, output, session) {
     ratios_plot <-
       ggplot(data = plot_ratios,
              mapping = aes(x = SAMPLINGDATE, y = WAARDE, colour = NAAM, group = MONSTERPUNTCODE)) +
-      geom_line() +
-      geom_point(size = 2.5) +
-      geom_point(data = current_ratio(), size = 5) +
+      geom_line(alpha = 0.7) +
+      geom_point(size = 2.5, alpha = 0.5) +
+      geom_point(data = current_ratio(), size = 3.5) +
       guides(size = FALSE) +
       facet_wrap(vars(RATIO), scales = 'free_y')
     
