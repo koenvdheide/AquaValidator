24811236a7d125fbdbae0b6b437e948f0daa199d
Koen van der Heide | Aqualysis <kheide@aqualysis.nl>
2023-07-14 13:58:11 +0200

    AAV-48 change color from red to yellow

--- Shiny/app.R
+++ Shiny/app.R
@@ -439,7 +439,7 @@ server <- function(input, output, session) {
        ) 
      )  %>% formatStyle(columns = 'LABNUMMER',
                         valueColumns = 'LABNUMMER',
-                        backgroundColor = styleEqual(current_result()$LABNUMMER, 'red',default = 'gray')
+                        backgroundColor = styleEqual(current_result()$LABNUMMER, 'yellow',default = 'gray')
                         ) #%>% formatSignif(columns = c(-2,-3), digits = 3) #nog kijken hoe we datums uitzonderen
 
    })
