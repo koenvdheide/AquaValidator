32e9fd04e15726b0f51e88cd8a1d3a18523fc198
Koen van der Heide | Aqualysis <kheide@aqualysis.nl>
2023-07-14 13:53:14 +0200

    AAV-109 disable formatSignif for now, first have to check how to exclude the date columns

--- Shiny/app.R
+++ Shiny/app.R
@@ -440,7 +440,7 @@ server <- function(input, output, session) {
      )  %>% formatStyle(columns = 'LABNUMMER',
                         valueColumns = 'LABNUMMER',
                         backgroundColor = styleEqual(current_result()$LABNUMMER, 'red',default = 'gray')
-                        ) %>% formatSignif(columns = c(-2,-3), digits = 3)
+                        ) #%>% formatSignif(columns = c(-2,-3), digits = 3) #nog kijken hoe we datums uitzonderen
 
    })
 
