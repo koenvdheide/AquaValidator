4e0b5da0f9c554ab8a206320fc5a4c3be0188795
Koen van der Heide | Aqualysis <kheide@aqualysis.nl>
2023-09-08 23:13:10 +0200

    AAV-190 added some (still boilerplate) tests names

--- tests/testthat/test-app.R
+++ tests/testthat/test-app.R
@@ -1,3 +1,18 @@
-test_that("multiplication works", {
+test_that("excel_reader() reads .xlsx and .xlx files and nothing else", {
+  expect_equal(2 * 2, 4)
+})
+test_that("excel_reader() understands different sheets", {
+  expect_equal(2 * 2, 4)
+})
+test_that("Samples() are loaded correctly", {
+  expect_equal(2 * 2, 4)
+})
+test_that("Results() are loaded correctly", {
+  expect_equal(2 * 2, 4)
+})
+test_that("ratio_calculator() calculates ratios correctly", {
+  expect_equal(2 * 2, 4)
+})
+test_that("ratio_calculator() calculated the right ratios", {
   expect_equal(2 * 2, 4)
 })
