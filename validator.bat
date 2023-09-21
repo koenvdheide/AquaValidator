4a76f2d0d5001b72a806a3a524cafd83d861c216
Koen van der Heide | Aqualysis <kheide@aqualysis.nl>
2023-09-21 11:50:08 +0200

    AAV-203 .bat script looks for dynamic path

--- validator.bat
+++ validator.bat
@@ -1,1 +1,4 @@
-"C:\Users\kheide\OneDrive - Aqualysis\R-4.2.3\bin\R.exe" CMD BATCH "run.r"
+@echo off 
+echo "Validator is aan het opstarten"
+echo "Sluit dit scherm om de validator af te sluiten"
+"%~dp0..\..\OneDrive - Aqualysis\R-4.2.3\bin\R.exe" CMD BATCH "run.r"
