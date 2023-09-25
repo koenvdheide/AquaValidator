8ef2e5a65c0f390291db7dd7330b00c5730a775d
Koen van der Heide | Aqualysis <kheide@aqualysis.nl>
2023-09-25 14:19:15 +0200

    AAV-203 clearer text in the batch script

--- validator.bat
+++ validator.bat
@@ -1,4 +1,5 @@
 @echo off 
-echo "Validator is aan het opstarten"
-echo "Sluit dit scherm om de validator af te sluiten"
+echo "Validator start op.. dit duurt een paar seconden"
+echo.
+echo "Sluit dit scherm om de validator vervolgens weer af te sluiten"
 "%~dp0..\..\OneDrive - Aqualysis\R-4.2.3\bin\R.exe" CMD BATCH "run.r"
